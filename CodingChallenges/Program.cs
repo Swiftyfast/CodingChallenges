c5322dafd1578634fe25926064e71b99047769af
Swiftyfast <jdavidswift@gmail.com>
2021-01-11 20:34:35 -0600

    Finished challenge 1.

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -11,6 +11,21 @@ namespace CodingChallenges
         static void Main(string[] args)
         {
             Console.WriteLine("Does this work");
+            long theSum = 0;
+            int whileLoopInt = 1;
+
+            while (whileLoopInt < 1000)
+            {
+                if (whileLoopInt % 3 == 0 || whileLoopInt % 5 == 0)
+                {
+                    theSum = theSum + whileLoopInt;
+                }
+
+                whileLoopInt++;
+            }
+
+            Console.WriteLine(theSum);
+            //I got 233168
         }
     }
 }
