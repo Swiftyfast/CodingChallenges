900ea297a398c8cf63f9711f477c65d61a8e5b91
Swiftyfast <jdavidswift@gmail.com>
2021-01-12 19:38:24 -0600

    Did the Tuesday

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -32,7 +32,26 @@ namespace CodingChallenges
             Console.WriteLine("");
 
             //TUESDAY
+            long tuesdaySum = 2;
+            long x = 1; //This will be the first number in the sequence
+            long y = 2; //This will be the second;
 
+            while (y <= 4000000)
+            {
+                long nextNumber = x + y;
+                if (nextNumber % 2 == 0)
+                {
+                    tuesdaySum = tuesdaySum + nextNumber;
+                }
+                x = y;
+                y = nextNumber;
+
+                //tuesdayCount++;
+            }
+
+            Console.WriteLine("The answer to Tuesday's problem is " + tuesdaySum);
+            //I got 4613732
+            Console.WriteLine("");
             //WEDNESDAY
 
             //THURSDAY
